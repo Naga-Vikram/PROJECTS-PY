f57e5a95004b2958353ce2de5102b1df03485230
Naga-Vikram <blvssnagavikram11@gmail.com>
2025-05-19 21:23:53 +0530

    Update drinking_water_remainder.py

--- drinking_water_remainder.py
+++ drinking_water_remainder.py
@@ -1,3 +1,4 @@
+"""This Python script uses the win10toast library to display a "Drink Water!" notification every 2 hours on Windows 10"""
 import winsound
 import os
 import time
@@ -6,7 +7,7 @@ from datetime import datetime
 while True:
     
     from win10toast import ToastNotifier
-    time.sleep(6)
+    time.sleep(7200)
     toaster = ToastNotifier()
     toaster.show_toast("Drink Water!", "It's time to hydrate.", duration=10)
     
