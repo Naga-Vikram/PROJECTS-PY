93e0c6ad394acee39cb7ebc4d5c0dd1e6677ee09
Naga-Vikram <blvssnagavikram11@gmail.com>
2025-05-19 21:13:46 +0530

    Update drinking_water_remainder.py

--- drinking_water_remainder.py
+++ drinking_water_remainder.py
@@ -3,27 +3,6 @@ import os
 import time
 from datetime import datetime
 
-# def notify(message, sound):
-#     print(f"[{datetime.now().strftime('%H:%M:%S')}] {message}")
-#     if sound == 'default':
-#         winsound.PlaySound("SystemDefault", winsound.SND_ALIAS)
-#     elif sound == 'asterisk':
-#         winsound.PlaySound("SystemAsterisk", winsound.SND_ALIAS)
-#     elif sound == 'exclamation':
-#         winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
-#     elif sound == 'hand':
-#         winsound.PlaySound("SystemHand", winsound.SND_ALIAS)
-#     elif sound == 'question':
-#         winsound.PlaySound("SystemQuestion", winsound.SND_ALIAS)
-#     elif sound:
-#         if os.path.exists(sound):
-#             winsound.PlaySound(sound, winsound.SND_FILENAME)
-#         else:
-#             print(f"Warning: Sound file '{sound}' not found.")
-
-# # Example usage:
-
-
 while True:
     
     from win10toast import ToastNotifier
